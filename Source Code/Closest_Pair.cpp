85463c9a1cbb2bf966279392c5a63eb26cad9bd9
Siwakorn <arsurarizen@gmail.com>
2017-03-11 18:40:23 +0700

    Update Closest_Pair.cpp

--- Source Code/Closest_Pair.cpp
+++ Source Code/Closest_Pair.cpp
@@ -5,35 +5,46 @@
 
 using namespace std;
 
-int distance(int vx1, int vy1, int vx2, int vy2)
+struct Point
 {
-    return sqrt(pow((vx1 - vx2), 2) + pow((vy1 - vy2), 2));
+    int x;
+    int y;
+};
+
+double distance(Point point1, Point point2)
+{
+    return sqrt(pow((point1.x - point2.x), 2) + pow((point1.y - point2.y), 2));
 }
 
-int main()
+void minimumDistance(Point point[], int no)
 {
-    int vx[100];
-    int vy[100];
-
-    ifstream readfile("Closest_Pair_Data", ios::in);
-
-    int no = 0;
-    while (readfile >> vx[no] >> vy[no]) {
-        ++no;
-    }
-
-    int minDistance = distance(vx[0], vy[0], vx[1], vy[1]);
+    Point p1, p2;
+    double minDistance = distance(point[0], point[1]);
     for (int i = 0; i < no; ++i) {
         for (int j = i + 1; j < no; ++j) {
-            cout << vx[i] << " : " << vy[i] << " and " << vx[j] << " : " << vy[j] << " == " << distance(vx[i], vy[i], vx[j], vy[j]) << endl;
-            if (minDistance >= distance(vx[i], vy[i], vx[j], vy[j])) {
-                minDistance = distance(vx[i], vy[i], vx[j], vy[j]);
+            cout << point[i].x << " : " << point[i].y << " and " << point[j].x << " : " << point[j].y << " == " << distance(point[i], point[j]) << endl;
+            if (minDistance >= distance(point[i], point[j])) {
+                minDistance = distance(point[i], point[j]);
+                p1 = point[i];
+                p2 = point[j];
             }
         }
     }
 
-    cout << endl;
-    cout << minDistance << endl;
+    cout << "\n" << p1.x << " : " << p1.y << " and " << p2.x << " : " << p2.y << endl;
+}
 
+int main()
+{
+    Point point[100];
+
+    ifstream readfile("Closest_Pair_Data.txt", ios::in);
+
+    int no = 0;
+    while (readfile >> point[no].x >> point[no].y) {
+        ++no;
+    }
+
+    minimumDistance(point, no);
 
 }
